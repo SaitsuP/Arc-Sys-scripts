a79f7eefb4e837cd8c42c42b9f3a191f8277d44c
dobosken <oscarmik@gmail.com>
2023-11-20 14:20:32 +0100

    Maze Walker

--- prepareAnimImports.py
+++ prepareAnimImports.py
@@ -1,17 +1,33 @@
-# Sets all animations to constant interpolation, and removes the object suffix from all NLA tracks & actions.
-# Select the armature in the outliner before running the script.
-
+# Sets all animations to constant interpolation.
+# Removes the object suffix from all NLA tracks & actions.
+# Clamps bone scale to prevent absolute zero (which can cause issues in UE4).
 import bpy
 
-obj = bpy.context.active_object
-actions = bpy.data.actions
-nlaTracks = obj.animation_data.nla_tracks
+def drawPanel(txt):
+    def draw(self, context):
+        self.layout.label(text=txt)
+    bpy.context.window_manager.popup_menu(draw, title = "prepareAnimImports", icon = "INFO")
+
+def grabObj():
+    try:
+        obj = bpy.context.active_object
+        match obj.type:
+            case "MESH":
+                if obj.parent and obj.parent.type == "ARMATURE":
+                    return obj.parent
+                else:
+                    return None
+            case "ARMATURE":
+                return obj
+            case _:
+                return None
+    except:
+        return None
 
 def setConstantInterpolation():
     for a in actions:
         try:
-            fcurves = a.fcurves
-            for fcurve in fcurves:
+            for fcurve in a.fcurves:
                 for kf in fcurve.keyframe_points:
                     kf.interpolation = 'CONSTANT'
         except:
@@ -31,6 +47,26 @@ def renameNlaStrips():
         except:
             pass
 
-setConstantInterpolation()
-renameActions()
-renameNlaStrips()
+def clampKeyframeScale():
+    for a in actions:
+        try:
+            for fcurve in a.fcurves:
+                if fcurve.data_path.endswith("scale"):
+                    for kf in fcurve.keyframe_points:
+                        if kf.co.y == 0:
+                            kf.co.y = 0.001
+        except:
+            pass
+
+obj = grabObj()
+if obj is None:
+    drawPanel("Select the proper armature/mesh first, please.")
+else:
+    actions = bpy.data.actions
+    nlaTracks = obj.animation_data.nla_tracks
+
+    setConstantInterpolation()
+    renameActions()
+    renameNlaStrips()
+    clampKeyframeScale()
+    drawPanel("Done :)")
